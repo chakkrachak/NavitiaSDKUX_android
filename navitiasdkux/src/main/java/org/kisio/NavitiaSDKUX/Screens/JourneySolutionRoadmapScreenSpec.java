cdd8a27cc6ae649f15541f3584a129317dc6c37c
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-11-27 17:13:00 +0100

    Removed optional in Props

--- navitiasdkux/src/main/java/org/kisio/NavitiaSDKUX/Screens/JourneySolutionRoadmapScreenSpec.java
+++ navitiasdkux/src/main/java/org/kisio/NavitiaSDKUX/Screens/JourneySolutionRoadmapScreenSpec.java
@@ -33,8 +33,8 @@ public class JourneySolutionRoadmapScreenSpec {
     @OnCreateLayout
     static ComponentLayout onCreateLayout(
         ComponentContext c,
-        @Prop(optional = true) Journey journey,
-        @Prop(optional = true) List<Disruption> disruptions) {
+        @Prop Journey journey,
+        @Prop List<Disruption> disruptions) {
 
         return BaseViewComponent.create(c).testKey("roadmap").child(
             ContainerComponent.create(c)
