6d75abdc74873c87ccef9e21571b30f62607484d
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-11-28 11:58:50 +0100

    Refactoring

--- navitiasdkux/src/main/java/org/kisio/NavitiaSDKUX/Components/Journey/Roadmap/Sections/Waiting/DescriptionComponentSpec.java
+++ navitiasdkux/src/main/java/org/kisio/NavitiaSDKUX/Components/Journey/Roadmap/Sections/Waiting/DescriptionComponentSpec.java
@@ -1,16 +1,20 @@
 package org.kisio.NavitiaSDKUX.Components.Journey.Roadmap.Sections.Waiting;
 
 import android.graphics.Color;
+import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.ComponentLayout;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
 import com.facebook.litho.annotations.Prop;
 
+import com.facebook.yoga.YogaAlign;
+import com.facebook.yoga.YogaJustify;
 import org.kisio.NavitiaSDK.models.Section;
-import org.kisio.NavitiaSDKUX.Components.Journey.Roadmap.Sections.Waiting.Description.WaitingIconComponent;
+import org.kisio.NavitiaSDKUX.Components.IconComponent;
 import org.kisio.NavitiaSDKUX.Components.Primitive.HorizontalViewComponent;
 import org.kisio.NavitiaSDKUX.Components.TextComponent;
+import org.kisio.NavitiaSDKUX.Components.ViewComponent;
 import org.kisio.NavitiaSDKUX.Config.Configuration;
 import org.kisio.NavitiaSDKUX.R;
 import org.kisio.NavitiaSDKUX.Util.Metrics;
@@ -29,15 +33,19 @@ class DescriptionComponentSpec {
 
         builder
             .child(
-                WaitingIconComponent.create(c)
+                ViewComponent.create(c)
                     .styles(iconContainerStyles)
-                    .build())
+                    .children(new Component<?>[] {
+                        IconComponent.create(c)
+                            .name("clock")
+                            .styles(iconStyles)
+                            .build()
+                }).build())
             .child(
                 TextComponent.create(c)
                     .styles(labelStyles)
                     .text(Metrics.durationText(c, section.getDuration()) + " " + c.getString(R.string.journey_roadmap_action_wait))
-                    .build()
-            );
+                    .build());
 
         return builder.build();
     }
@@ -45,6 +53,14 @@ class DescriptionComponentSpec {
     static Map<String, Object> iconContainerStyles = new HashMap<>();
     static {
         iconContainerStyles.put("width", 50);
+        iconContainerStyles.put("alignItems", YogaAlign.CENTER);
+        iconContainerStyles.put("justifyContent", YogaJustify.CENTER);
+    }
+
+    static Map<String, Object> iconStyles = new HashMap<>();
+    static {
+        iconStyles.put("color", Configuration.colors.getGray());
+        iconStyles.put("fontSize", 20);
     }
 
     static Map<String, Object> labelStyles = new HashMap<>();
